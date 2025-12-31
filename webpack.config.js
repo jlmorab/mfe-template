9226e2300d9730ed630274dfca4e8ea892f6d4f1
José Luis Mora Barrios <jlmorab@hotmail.com>
2025-12-31 02:10:12 -0600

    Baseline

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'mfe-template',
+
+  exposes: {
+    './Component': './src/app/app.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+});
